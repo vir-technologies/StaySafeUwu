6e974ada51980f6272f3eba69cc2cac835a5b3e9
vangeliq <valeryangelique2001@gmail.com>
2021-05-28 22:55:03 +0700

    added placeholder widget class

---
+++ hackon/lib/widgets/PlaceholderWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// this is a placeholder for the bottom menu page (for now)
+class PlaceholderWidget extends StatelessWidget {
+  final Color color;
+
+  PlaceholderWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+    );
+  }
+}
